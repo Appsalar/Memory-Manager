6cbe39e98d9d80b7e83d921a0566c39877e75d3c
Ivan Velkov <sasmose@yahoo.com>
2016-07-07 15:12:33 +0300

    fix add

---
+++ sdp_project/TempAllocator.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "../Allocator/Allocator.h"
+
+template <class T>
+class TempAllocator
+{
+public:
+	typedef size_t    size_type;
+	typedef ptrdiff_t difference_type;
+	typedef T*        pointer;
+	typedef const T*  const_pointer;
+	typedef T&        reference;
+	typedef const T&  const_reference;
+	typedef T         value_type;
+
+public:
+	TempAllocator()
+	{
+		pMem = new Allocator(1000 * 1000);
+	}
+
+	TempAllocator(size_type size)
+	{
+		pMem = new Allocator(size / 8);
+	}
+
+	TempAllocator(const TempAllocator& other)
+	{
+		pMem = other.pMem;
+	}
+
+	template <class U> TempAllocator(const TempAllocator<U>& other)
+	{
+		pMem = other.pMem;
+	}
+
+	~TempAllocator() {}
+
+	pointer allocate(size_type size)
+	{
+		pointer ptr = (pointer)pMem->MyMalloc(size * sizeof(value_type));
+		return ptr;
+	}
+
+	void deallocate(pointer p, size_type n)
+	{
+		pMem->MyFree((char*)p);
+	}
+public:
+	Allocator* pMem;
+};
+
+
+template <class T1, class T2>
+bool operator==(const TempAllocator<T1>&, const TempAllocator<T2>&)
+{
+	return false;
+}
+
+template <class T1, class T2>
+bool operator!=(const TempAllocator<T1>&, const TempAllocator<T2>&)
+{
+	return true;
+}
